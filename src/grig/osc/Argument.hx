f7b9bec0b97e4a58c527976cbde3667d81bc38a7
Thomas J. Webb <thomas@osakared.com>
2020-08-29 18:59:16 -0700

    Added travis configuration

--- src/grig/osc/Argument.hx
+++ src/grig/osc/Argument.hx
@@ -1,5 +1,7 @@
 package grig.osc;
 
+using thx.Int64s;
+
 class Argument
 {
     public var val(default, null):Any;
@@ -13,6 +15,14 @@ class Argument
 
     public function toString():String
     {
-        return '$type $val';
+        // This code is brittle and confusing. Basically, it's to help tests pass because Int64 looks different in different targets
+        // All the types that store as Int64 so that the tests pass. There needs to be a better way to do that.
+        var displayVal:String = if ([ArgumentType.Int64, ArgumentType.Color, ArgumentType.Midi].contains(type)) {
+            var i:Int64 = cast val;
+            i.toStr();
+        } else {
+            '${this.val}';
+        }
+        return '$type $displayVal';
     }
 }
